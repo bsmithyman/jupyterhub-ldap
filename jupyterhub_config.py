f751d9642c71b8a14d69b19413fd76a5cda85899
Brendan Smithyman <brendan@bitsmithy.net>
2015-12-18 15:48:27 -0500

    Add support to pass through environment variables (e.g., links).

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -8,3 +8,7 @@ if os.path.exists(keyfile) and os.path.exists(certfile):
     c.JupyterHub.ssl_key = keyfile
     c.JupyterHub.ssl_cert = certfile
     c.JupyterHub.port = 443
+
+c.Spawner.env_keep += [name for name in os.environ.get('ENVPASSWHITELIST','').sp
+lit(',')]
+
